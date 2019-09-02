9de71c8e6798d596ea749ee53309eedadaeaf011
Gustavo dos Santos reis <gustavoreis567@gmail.com>
2019-09-02 19:30:46 -0300

    1 Eesqueleto

---
+++ Agenda/Classes/Compromisso.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Classes
+{
+    public class Compromisso
+    {
+        public String Titulo { get; set; }
+        [Required(ErrorMessage = "O campo Titulo é requerido!!")]
+        [MaxLength(200, ErrorMessage = "O campo Titulo recebe no máximo 200 caracteres")]
+        [Display(Name = "Sobrenome")]
+        public String Local { get; set; }
+        [Required(ErrorMessage = "O campo Local é requerido!!")]
+        [MaxLength(100, ErrorMessage = "O campo Sobrenome recebe no máximo 100 caracteres")]
+        [Display(Name = "Sobrenome")]
+        public DateTime Inicio { get; set; }
+        public DateTime Fim { get; set; }
+        public IList<Contato> Participantes { get; set; }
+    }
+}
---
+++ Agenda/Classes/Contato.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agenda.Classes
+{
+    public class Contato
+    {
+        public String Nome { get; set; }
+        [Required(ErrorMessage = "O campo Nome é requerido!!")]
+        [MaxLength(100, ErrorMessage = "O campo Nome recebe no máximo 100 caracteres")]
+        [Display(Name = "Nome")]
+        public String Sobrenome { get; set; }
+        [Required(ErrorMessage = "O campo Sobrenome é requerido!!")]
+        [MaxLength(100, ErrorMessage = "O campo Sobrenome recebe no máximo 100 caracteres")]
+        [Display(Name = "Sobrenome")]
+        public DateTime Nasc { get; set; }
+        public String Telefone { get; set; }
+        [Required(ErrorMessage = "O campo Telefone é requerido!!")]
+        [MaxLength(20, ErrorMessage = "O campo Telefone recebe no máximo 20 caracteres")]
+        [Display(Name = "Telefone")]
+        public IList<Compromisso> Compromissos { get; set; }
+    }
+}
